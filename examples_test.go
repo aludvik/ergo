4dd2ece0838097f9c7a1a630af2ca9d81ee552ca
Adam M Ludvik <adam.m.ludvik@gmail.com>
2019-08-11 20:54:12 -0700

    Fix example suffixes
    go vet does not like numbers

--- examples_test.go
+++ examples_test.go
@@ -7,14 +7,14 @@ import (
 )
 
 
-func Example_1() {
+func Example_simple() {
   ergo.New(1).
     Map(func(i int) int { return i + 1 }).
     AndThen(func(i int) ergo.Result { return ergo.New(errors.New("error")) }).
     WrapErr("wrapper")
 }
 
-func Example_2() {
+func Example_compatibility() {
   v, err := ergo.FromPair(nil, errors.New("error")).
     MapErr(func(error) error { return errors.New("new error") }).
     IntoPair()
